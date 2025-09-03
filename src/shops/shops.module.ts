1c89866771afe95eaac99c5e625ad0eec764be61
Umarta2023 <gizatrustam@gmail.com>
2025-09-03 21:44:57 +0500

    fix: register Shop entity in ShopsModule

--- src/shops/shops.module.ts
+++ src/shops/shops.module.ts
@@ -1,8 +1,12 @@
+// ПРАВИЛЬНО
 import { Module } from '@nestjs/common';
 import { ShopsService } from './shops.service';
 import { ShopsController } from './shops.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shop } from './entities/shop.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Shop])], // <-- ДОБАВИТЬ ЭТУ СТРОКУ
   controllers: [ShopsController],
   providers: [ShopsService],
 })
