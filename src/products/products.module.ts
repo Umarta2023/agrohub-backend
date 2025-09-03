19efeadcae2294f5d2a865572bfa549abf9ca66d
Umarta2023 <gizatrustam@gmail.com>
2025-09-03 21:48:36 +0500

    fix: register entities in all modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm'; // <-- Импортировать
+import { Product } from './entities/product.entity'; // <-- Импортировать
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])], // <-- Добавить эту строку
   controllers: [ProductsController],
   providers: [ProductsService],
 })
